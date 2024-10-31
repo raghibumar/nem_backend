c961f4f20d28c79a298fb80645cecf40b27cc0ed
ABU RAGHIB UMAR <raghibumar2@gmail.com>
2024-10-31 20:20:27 +0530

    change

--- configs/db.js
+++ configs/db.js
@@ -2,8 +2,15 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 
 const dbUrl = process.env.DATABASE_URL;
+if (!dbUrl) {
+  console.error("DATABASE_URL environment variable is not set.");
+  process.exit(1); // Exit with an error code if database URL is missing
+}
 
-const connection = mongoose.connect(dbUrl);
+const connection = mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 module.exports = {
   connection,
